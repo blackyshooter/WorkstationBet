2fe27d405b4ed20c8e5f9993f1b9e249ad0da5d8
blackyshooter <elias.soto.py@outlook.com>
2024-02-23 16:02:23 -0300

    Leccion1

--- Codegym/Leccion1/EntryPoint.java
+++ Codegym/Leccion1/EntryPoint.java
@@ -1,5 +1,7 @@
 package mx.com.codegym.lesson01;
 
+import java.util.Scanner;
+
 /*
     Esta será la clase que arrancará nuestro sistema
     de la empresa Codegym
@@ -7,11 +9,11 @@ package mx.com.codegym.lesson01;
 public class EntryPoint {
 
     public static void main(String[] args) {
-
         Empleado jordan = new Empleado();
         jordan.nombre = "Jordan Alberto";
         jordan.idEmpleado  = 1;
         jordan.correoElectronico = "jordan.saenz@codegym.com";
+        System.out.println(jordan.nombre);
     }
 
 }
